1de926514f833bc8bce3165843ffc118773f7c4d
Rahul Raj <rahul_raj@newgensoft.com>
2026-05-10 19:43:31 +0530

    IC3

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
 import com.example.demo.service.UserService;
@@ -11,6 +12,7 @@ public class UserController {
 
     private final UserService service;
 
+    @Autowired
     public UserController(UserService service) {
         this.service = service;
     }
